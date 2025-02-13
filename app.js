22bad1e6132cf7fc388e66f0ede3a54f9c71afdb
andresyarte <83657455+andresyarte@users.noreply.github.com>
2025-02-13 14:04:25 -0600

    Add various new files and configurations for dependencies and modules

--- app.js
+++ app.js
@@ -67,14 +67,14 @@ app.get('/people', async (req, res) => {
   }
 });
 
-mongoose.connect(
-  'mongodb://localhost:27017/swfavorites',
-  { useNewUrlParser: true },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(3000);
-    }
-  }
-);
+mongoose.connect('mongodb://localhost:27017/swfavorites', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Conectado a MongoDB');
+  app.listen(3000, () => {
+      console.log('Servidor corriendo en el puerto 3000');
+  });
+}).catch(err => {
+  console.error('Error al conectar a MongoDB:', err);
+});
